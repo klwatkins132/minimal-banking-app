49a0c951fcabc01ab5cc50e6eb2127f197f70cba
Kelsy Watkins <klwatkins132@gmail.com>
2022-08-09 16:23:37 -0600

    place comment todo's for future me

--- script.js
+++ script.js
@@ -3,3 +3,22 @@
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // BANKIST APP
+
+// todo: make some preset accounts
+
+// todo: target html elements
+
+// todo: Functions
+// display and calc balance
+// display and calc summary -- in, out, interest
+// display movements -- deposits, withdrawls, date, value
+// create usrnames for accounts
+// logout timer
+
+// todo: Event Handlers
+// hello user on login -- start timer
+// transfer money
+// loan money
+// close account
+// hide main
+// sort movements
